fcc900f192b1455936851c136b105760da77c8ca
Million Integrals <jerry@millionintegrals.com>
2014-12-12 14:25:54 +0000

    Improved a bit demo side of the project

---
+++ scripts/demo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! [ -d output ]; then
+    mkdir output
+fi
+
+echo "Encoding marocco..."
+python3 -m encoder.main encode -i data/marocco.jpg -o output/marocco_encoded.png -d data/ALICE_IN_WONDERLAND --intensity=2 --block_size=64 
+echo "Decoding marocco..."
+python3 -m encoder.main decode -i output/marocco_encoded.png -o output/marocco_decoded.txt --intensity=2 --block_size=64 
+
+echo "Encoding dragon..."
+python3 -m encoder.main encode -i data/dragon.jpg -o output/dragon_encoded.png -d data/ALICE_IN_WONDERLAND --intensity=4 --block_size=8 
+echo "Decoding dragon..."
+python3 -m encoder.main decode -i output/dragon_encoded.png -o output/dragon_decoded.txt --intensity=4 --block_size=8 
+
+echo "Done"
